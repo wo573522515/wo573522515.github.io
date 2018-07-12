31c2e77a946d8286cb2f92adae630a0a65b6b7ca
Your Name <you@example.com>
2018-07-12 21:50:51 +0800

    ladash

--- miao/lodash/wo573522515-lodash.js
+++ miao/lodash/wo573522515-lodash.js
@@ -31,5 +31,18 @@ var wo573522515 = {
             }
         }
         return array
-    }
+    },
+    differenceBy:function differenceBy(array,values,iteratee){
+        let k =[]
+        for(let i = 1;i<arguments.length-1;i++){
+            for(let p = 0;p<arguments[i].length;p++)
+            k=k.concat(iteratee(arguments[i][p]))
+        }
+        for(let j =0;j<array.length;j++){
+            if(k.indexOf(iteratee(array[j])) !== -1){
+                array.splice([j],1)
+            }
+        }
+        return array
+    },
 }
