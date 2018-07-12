b918d6af797298c4f2d5b368736a6528b7664bae
Your Name <you@example.com>
2018-07-12 16:16:01 +0800

    lodash

--- miao/lodash/wo573522515-lodash.js
+++ miao/lodash/wo573522515-lodash.js
@@ -1,3 +1,14 @@
 var wo573522515 = {
-
+    chunk:  function chunk(array,size) {
+        let k=[]
+        let s=[]
+        for(let i = 0;i < array.length;i++){
+            k.push(array[i])
+            if(i !==0 && (i+1) % size === 0 ||i ==array.length-1){
+                s.push(k)
+                k = []
+            }
+        }
+        return s
+    }
 }
